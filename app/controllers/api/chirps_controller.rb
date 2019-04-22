8ffba3662a385a1461eeedb86350764511ad626b
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2019-04-22 15:25:40 -0700

    Trim controllers and update routes

---
+++ app/controllers/api/chirps_controller.rb
@@ -0,0 +1,22 @@
+class Api::ChirpsController < ApplicationController
+
+  def index
+    chirps = Chirp.all
+    render json: chirps
+  end
+
+  def create
+    chirp = Chirp.new(chirp_params)
+    chirp.user_id = User.all.sample.id
+
+     if chirp.save
+      render json: chirp
+    else
+      render json: chirp.errors.full_messages, status: 422
+    end
+  end
+
+  def chirp_params
+    params.require(:chirp).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get request to /users maps to the index action in the users controller
-  # get '/users', to: 'users#index'
-  # get '/users/:id', to: 'users#show'
-  # post '/users', to: 'users#create'
-  # get '/custom', to: 'users#cheese' # we can do this too!
-
-  # can do except instead of only
-  resources :users do
-    # maybe we have a form to create a chirp on the user show page
-    resources :chirps, only: [:create, :index]
+  namespace :api, defaults: { format: :json } do
+    resources :chirps, only: [:index, :create]
   end
 
-  resource :session, only: [:new, :create, :destroy]
-
-  resources :chirps, only: [:show]
+  root to: 'static_pages#root'
 
 end
